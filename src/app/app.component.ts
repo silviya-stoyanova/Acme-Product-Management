2c7d46244d4f98a259344e5fc2b8ee2c4ae8bd6a
Siviya Stoyanova <silvia.s.stoianova@gmail.com>
2023-10-05 23:18:58 +0300

    Create the project with the main views, assets and styles

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: '#root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+
+export class AppComponent {
+  title = 'Acme Product Management';
+}
