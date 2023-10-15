f10eaaa7987bd316dba9263aa0273f68a2fc0e58
Siviya Stoyanova <silvia.s.stoianova@gmail.com>
2023-10-15 21:39:31 +0300

    Implement desktop styles. Add variables for the colors.

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -5,7 +5,24 @@ import { Component } from '@angular/core';
   templateUrl: './product-list.component.html',
   styleUrls: ['product-list.component.scss'],
 })
-
 export class ProductListComponent {
-  filter: string = '';
+  filter: string = 'dsa';
+  productList: Array<any> = [
+    {
+      imageUrl: 'assets/images/hammer.png',
+      name: 'Hammer',
+      code: 'tbx 0048',
+      available: 'May 21, 2021',
+      price: '$8.90',
+      rating: '4.7',
+    },
+    {
+      imageUrl: 'assets/images/hammer.png',
+      name: 'Hammer 2',
+      code: 'tbx 0048 2',
+      available: 'May 22, 2022',
+      price: '$2.22',
+      rating: '2.2',
+    },
+  ];
 }
