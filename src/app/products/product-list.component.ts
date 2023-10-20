02cb5e3261cbd5d8dbec261af961fa1cf0704533
Siviya Stoyanova <silvia.s.stoianova@gmail.com>
2023-10-20 23:44:40 +0300

    Implement OnInit lifecycle hook for the ProductListComponent

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
 
 @Component({
@@ -6,7 +6,7 @@ import { IProduct } from './product';
   templateUrl: './product-list.component.html',
   styleUrls: ['product-list.component.scss'],
 })
-export class ProductListComponent {
+export class ProductListComponent implements OnInit {
   pageTitle: string = 'Product List';
   filter: string = '';
   showImage: boolean = false;
@@ -36,5 +36,8 @@ export class ProductListComponent {
   toggleImage(): void {
     this.showImage = !this.showImage;
     this.toggleImageBtnText = this.showImage ? 'Hide Image' : 'Show Image';
+  }
+  ngOnInit(): void {
+    console.log('ProductListComponent was initialized.');
   }
 }
