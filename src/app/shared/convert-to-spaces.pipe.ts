eac0414942037b8c87829a763ee83b898cfd97f4
Siviya Stoyanova <silvia.s.stoianova@gmail.com>
2023-10-21 00:40:24 +0300

    Create a custom pipe to convert given symbol to space

---
+++ src/app/shared/convert-to-spaces.pipe.ts
@@ -0,0 +1,10 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'convertToSpacesPipe',
+})
+export class ConvertToSpacesPipe implements PipeTransform {
+  transform(value: string, character: string): string {
+    return value.replace(character, ' ');
+  }
+}
